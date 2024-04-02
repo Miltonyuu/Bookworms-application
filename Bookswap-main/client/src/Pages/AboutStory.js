bf3589a2b31752c02780d9b0fe0089e49f847efc
Milton Bautista <milton_bautista@yahoo.com>
2024-04-03 05:44:46 +0800

    Updates to aboutstory.js
    Center and change the color of the font for Our Story

--- Bookswap-main/client/src/Pages/AboutStory.js
+++ Bookswap-main/client/src/Pages/AboutStory.js
@@ -1,28 +1,30 @@
 import React from "react";
-import Header from "../Components/Header"; 
-import { Box, Heading, Text, Container } from "@chakra-ui/react";
+import Header from "../Components/Header";
+import { Box, Heading, Text, Container, useColorModeValue } from "@chakra-ui/react";
 
 const AboutStory = () => {
   return (
     <div>
-      <Header /> 
-      <Container maxW='container.md' p={12}> 
-        <Box 
-          bg="white"  
+      <Header />
+      <Container maxW="container.md" p={12}>
+        <Box
+          bg="white"
           width="100%"
           height="400px"
           display="flex"
           flexDirection="column"
           justifyContent="center"
-          alignItems="start"  
-          padding="30px" 
-          boxShadow="0px 2px 10px rgba(0, 0, 0, 0.1)"  
-          borderRadius="50px"  
+          alignItems="center" // Change: Center the items horizontally
+          padding="30px"
+          boxShadow="0px 2px 10px rgba(0, 0, 0, 0.1)"
+          borderRadius="50px"
         >
-          <Heading mb={4}>Our Story</Heading>
-          <Text lineHeight="1.5"> 
+          <Heading mb={4} color={useColorModeValue("teal.500", "yellow.400")}>
+            Our Story
+          </Heading>
+          <Text lineHeight="1.5" textAlign="center"> 
             We are fourth-year BSIS students at the Technological University of the Philippines 
-            developing BookSwap as our thesis project.  Driven by our passion for books and technology, 
+            developing BookSwap as our thesis project. Driven by our passion for books and technology, 
             we aim to create a platform for avid readers to exchange and discover new literary treasures.
           </Text>
         </Box>
