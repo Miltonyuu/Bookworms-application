15b79951bbb7d168712ea0e0e5050f1bdd0ed6b9
JOMAR CATALINO <catalinoj09@gmail.com>
2024-04-11 00:36:18 +0800

    Image slide
    image carousel or slider has been added

--- Bookswap-main/client/src/Pages/Home.js
+++ Bookswap-main/client/src/Pages/Home.js
@@ -3,6 +3,14 @@ import Header from '../Components/Header'
 import YourListings from '../Components/YourListings'
 import SearchBar from '../Components/SearchBar'
 import { useAuth } from '../Hooks/useAuth';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import "../Pages/slider.css";
+
+import img1  from "../Images/rezimage1.jpg";
+import img2  from "../Images/rezimage2.jpg";
+import img3  from "../Images/rezimage3.jpg";
 
 function Home() {
     const {isAuthenticated} = useAuth()
@@ -14,9 +22,22 @@ function Home() {
                 <SearchBar />
             </div>
             {isAuthenticated && <YourListings />}
-            
+
+            <div className="card">
+                <Carousel className='main-carousel'>
+                    <div>
+                        <img src={img1} style={{height:"500px", width:"2000px"}} />
+                    </div>
+                    <div>
+                        <img src={img2} style={{height:"500px", width:"2000px"}} />
+                    </div>
+                    <div>
+                        <img src={img3} style={{height:"500px", width:"2000px"}} />
+                    </div>
+                </Carousel>
+            </div>
+
         </div>
-        
     );
 }
 
