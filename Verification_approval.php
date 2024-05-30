963db55b380442b49fd59e0e7d9d0e0b0fcc53dd
JOMAR CATALINO <catalinoj09@gmail.com>
2024-05-31 06:06:47 +0800

    minor update in verification_approval.php and adminstyle.css
    minor update in verification_approval.php and adminstyle.css

--- Verification_approval.php
+++ Verification_approval.php
@@ -19,28 +19,30 @@ if(isset($_POST['update_request'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+
+        <!-- font awesome cdn link  -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+        <!-- custom admin css file link  -->
+        <link rel="stylesheet" href="css/admin-section/admin_style.css">
+    </head>
 <body>
     <?php include 'admin_header.php'; ?>
 
     <section class="verification-requests">
         <h1 class="title">Verification Requests</h1>
-           <!-- font awesome cdn link  -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-        <!-- custom admin css file link  -->
-        <link rel="stylesheet" href="css/admin-section/admin_style.css">
         <div class="box-container">
             <?php
             $select_requests = mysqli_query($conn, "SELECT * FROM `verification_requests` WHERE status = 'pending'") or die('query failed');
             if(mysqli_num_rows($select_requests) > 0){
                 while($fetch_requests = mysqli_fetch_assoc($select_requests)){
             ?>
-            <div class="box">
+            <div class="verify-box">
                 <p> user id : <span><?php echo $fetch_requests['user_id']; ?></span> </p>
                 <img src="uploaded_ids/<?php echo $fetch_requests['valid_id']; ?>" alt="">
                 <form action="" method="POST">
                     <input type="hidden" name="request_id" value="<?php echo $fetch_requests['id']; ?>">
-                    <select name="update_verification" class="box">
+                    <select name="update_verification" class="verification-box">
                         <option value="" selected disabled>Pending</option>
                         <option value="approved">Approve</option>
                         <option value="rejected">Reject</option>
@@ -51,7 +53,7 @@ if(isset($_POST['update_request'])){
             <?php
                 }
             }else{
-                echo '<p class="empty">no verification requests yet!</p>';
+                echo '<div><p class="empty" style="justify-content:center">no verification requests yet!</p></div>';
             }
             ?>
         </div>
